b0bf410e8cb99d49b8c82cc13f5e586402298804
Crristian Quesada Cossio <crisquesadaco@gmail.com>
2023-05-20 12:25:38 -0500

    fix: Fix mysql connection

--- index.js
+++ index.js
@@ -29,22 +29,13 @@ function getConnection(callback) {
   });
 }
 
-// Conexión a la base de datos MySQL
-connection.connect((err) => {
-  if (err) {
-    console.error("Error al conectar a la base de datos: ", err);
-  } else {
-    console.log("Conexión exitosa a la base de datos");
-  }
-});
-
 // Iniciar el servidor
 app.listen(port, () => {
   console.log("Servidor iniciado en el puerto " + port);
 });
 
 app.get("/", (req, res) => {
-    res.send("Hola mundo");
+  res.send("Hola mundo");
 });
 
 app.get("/emails", (req, res) => {
